ed4df1ad7a4081c55343db52820b641313bf4ef9
SpiritGun91 <spiritgun@vivaldi.net>
2025-05-02 06:56:14 -0400

    Add support for Food items in World class

--- class/world.js
+++ class/world.js
@@ -1,63 +1,55 @@
-const { Room } = require('./room');
-const { Item } = require('./item');
-const { Food } = require('./food');
+const { Room } = require("./room");
+const { Item } = require("./item");
+const { Food } = require("./food");
 
 class World {
-    constructor() {
-        this.rooms = {};
-    }
+	constructor() {
+		this.rooms = {};
+	}
 
-    loadWorld(worldData) {
+	loadWorld(worldData) {
+		const roomList = worldData.rooms;
+		const itemList = worldData.items;
 
-        const roomList = worldData.rooms;
-        const itemList = worldData.items;
+		// Instantiate new room objects
+		// Get name, id and description from room data
+		for (let i = 0; i < roomList.length; i++) {
+			const roomData = roomList[i];
+			const newRoom = new Room(roomData.name, roomData.description);
 
-        // Instantiate new room objects
-        // Get name, id and description from room data
-        for (let i = 0 ; i < roomList.length ; i++) {
+			this.rooms[roomData.id] = newRoom;
+		}
 
-            let roomData = roomList[i];
-            let newRoom = new Room(roomData.name, roomData.description);
+		// Connect rooms by ID
+		// Note that all rooms must be created before they can be connected
+		for (let i = 0; i < roomList.length; i++) {
+			const roomID = roomList[i].id;
+			const roomConnections = roomList[i].exits;
 
-            this.rooms[roomData.id] = newRoom;
-        }
+			for (const direction in roomConnections) {
+				const connectedRoomID = roomConnections[direction];
+				const roomToConnect = this.rooms[connectedRoomID];
+				this.rooms[roomID].connectRooms(direction, roomToConnect);
+			}
+		}
 
-        // Connect rooms by ID
-        // Note that all rooms must be created before they can be connected
-        for (let i = 0 ; i < roomList.length ; i++) {
+		// Instantiate items
+		for (let i = 0; i < itemList.length; i++) {
+			const itemData = itemList[i];
+			let newItem;
 
-            let roomID = roomList[i].id;
-            let roomConnections = roomList[i].exits;
+			if (itemData.isFood) {
+				// Fill this in
+				newItem = new Food(itemData.name, itemData.description);
+			}
+			newItem = new Item(itemData.name, itemData.description);
 
-            for (const direction in roomConnections) {
-                let connectedRoomID = roomConnections[direction];
-                let roomToConnect = this.rooms[connectedRoomID];
-                this.rooms[roomID].connectRooms(direction, roomToConnect);
-            }
-
-        }
-
-        // Instantiate items
-        for (let i = 0 ; i < itemList.length ; i++) {
-
-            let itemData = itemList[i];
-            let newItem;
-
-            if (itemData.isFood) {
-                console.log("ERROR: Food not supported yet.");
-                // Fill this in
-                return;
-            } else {
-                newItem = new Item(itemData.name, itemData.description);
-            }
-
-            let itemRoom = this.rooms[itemData.room];
-            itemRoom.items.push(newItem);
-       }
-
-    }
+			const itemRoom = this.rooms[itemData.room];
+			itemRoom.items.push(newItem);
+		}
+	}
 }
 
 module.exports = {
-  World,
+	World,
 };
