24909c15877a275c3f0108305ed1613e51ddc72e
SpiritGun91 <spiritgun@vivaldi.net>
2025-05-02 07:09:13 -0400

    Implement Food class with isFood property

--- class/food.js
+++ class/food.js
@@ -2,7 +2,12 @@ const { Item } = require("./item");
 
 // FILL THIS OUT
 
-class Food extends Item {}
+class Food extends Item {
+	constructor(name, description) {
+		super(name, description);
+		this.isFood = true;
+	}
+}
 
 module.exports = {
 	Food,
